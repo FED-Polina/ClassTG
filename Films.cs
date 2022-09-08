be93c77a137a3b34c8d60e7a7d6069325073f237
Polina Fedulova <FEDPolina_A@bk.ru>
2022-09-08 12:53:28 +0300

    ini

---
+++ Films.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    internal class Films
+    {
+        public string ID { get; set; }//Уникальный айди пользователя
+        public string Name { get; set; }//Название фильма
+        public string Genre { get; set; }//Жанр
+        public string Year { get; set; }//Год выпуска
+        public string AgeLimit { get; set; }//Возрастное ограничение
+        public string Lasting { get; set; }//Продолжительность
+        public string Description { get; set; }//Описание
+        public string URL { get; set; }//Ссылка
+    }
+}
